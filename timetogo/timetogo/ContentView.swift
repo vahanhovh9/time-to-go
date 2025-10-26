638b1167809cba53b0b7ec375db3760a779cde0b
vahanhovh9 <vahan.hovh@gmail.com>
2025-10-26 19:46:43 +0000

    feat: initial Xcode project setup

---
+++ timetogo/timetogo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  timetogo
+//
+//  Created by Vahan Hovhannisyan on 26/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ timetogo/timetogo/timetogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  timetogoApp.swift
+//  timetogo
+//
+//  Created by Vahan Hovhannisyan on 26/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct timetogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
